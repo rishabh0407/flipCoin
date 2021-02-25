6f4b9224b125ce29724d39a3935a94994dee2a17
rishabh0407 <rishabhinvincible123@gmail.com>
2021-02-25 08:43:44 +0530

    Modified the code for 21 times head or tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,13 +2,23 @@
 
 isHead=1;
 isTail=0;
-for(( i=0; i<=10; i++ ))
+a=1;
+b=1;
+while (( $a <= 21 || $b <= 21 ))
 do
   toss=$(( RANDOM % 2 ));
   if(( $toss == $isHead ));
   then
     echo "Head"
+    (( a++ ))
   else 
     echo "Tails"
+    (( b++ ))
   fi
 done
+if (( a == 21 ));
+then
+    echo "Heads is a winner"
+else
+    echo "Tails is a winner"
+fi
