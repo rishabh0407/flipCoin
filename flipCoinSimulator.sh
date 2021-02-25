292f1e6eccee337900dc63850e5fa9a807bd3eae
rishabh0407 <rishabhinvincible123@gmail.com>
2021-02-25 09:02:38 +0530

    Altered the code

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,7 +4,8 @@ isHead=1;
 isTail=0;
 a=1;
 b=1;
-while (( $a <= 21 || $b <= 21 ))
+i=1;
+while (( $i <= 20  ))
 do
   toss=$(( RANDOM % 2 ));
   if(( $toss == $isHead ));
@@ -15,10 +16,36 @@ do
     echo "Tails"
     (( b++ ))
   fi
+  (( i++ ))
 done
-if (( a == 21 ));
+if (( $a > $b ));
 then
     echo "Heads is a winner"
+elif (( a == b ))
+then
+    sum1=(( $a - $b ));
+    sum2=(( $b - $a ));
+    while (( $sum1 == 2 || $sum2 == 2 ))
+do
+  toss=$(( RANDOM % 2 ));
+  if(( $toss == $isHead ));
+  then
+    echo "Head"
+    (( a++ ))
+  else
+    echo "Tails"
+    (( b++ ))
+  fi
+   if(( $sum1 == 2 ));
+   then
+      echo "Heads is a winner"
+   elif(( $sum2 == 2 ))
+   then
+      echo "Tails is a winner"
+   fi 
+
+done
 else
     echo "Tails is a winner"
 fi
+
